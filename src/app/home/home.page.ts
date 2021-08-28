b51329ce906a014316a0bd3b698e5d53556d306a
Leonardo Galindo <61255738+GSLeonardo@users.noreply.github.com>
2021-08-28 15:47:54 -0500

    Camera Prototype
    Button and function to take picture

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  photo: SafeResourceUrl
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false, 
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+  this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl))
+
+  }
 
 }
